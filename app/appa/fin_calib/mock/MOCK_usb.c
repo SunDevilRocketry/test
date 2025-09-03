ca3c20b14bed327fa1fc695e318675db60345034
Dexx Stexx <drewpad@icloud.com>
2025-09-02 17:36:20 -0700

    Write tests for USB command code paths

--- app/appa/fin_calib/mock/MOCK_usb.c
+++ app/appa/fin_calib/mock/MOCK_usb.c
@@ -9,12 +9,30 @@
 *
 *******************************************************************************/
 
+/*------------------------------------------------------------------------------
+Project Includes
+------------------------------------------------------------------------------*/
 #include "main.h"
 #include "usb.h"
+#include "test.h"
 
+/*------------------------------------------------------------------------------
+Standard Includes
+------------------------------------------------------------------------------*/
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 
+/*------------------------------------------------------------------------------
+Global Variables
+------------------------------------------------------------------------------*/
+/* Queue of simulated USB transmissions, defined in test_fin_calib.c */
+extern uint8_t* usb_queue;
+extern int  usb_pos;
+
+/*------------------------------------------------------------------------------
+Mock Functions
+------------------------------------------------------------------------------*/
 bool usb_detect
 	(
 	void
@@ -25,10 +43,19 @@ bool usb_detect
 
 USB_STATUS usb_receive 
 	(
-	void*    rx_data_ptr ,
+	void*    rx_data_ptr,
 	size_t   rx_data_size,
 	uint32_t timeout
 	)
 {
+	uint8_t* result = rx_data_ptr;
+
+	/* If you get an out-of-bounds error here, it's probably because you forgot to
+	include an EXIT command in usb_queue or reset
+	usb_pos to 0. */
+	*result = usb_queue[ usb_pos ]; 
+
+	usb_pos++;
+	
 	return USB_OK;
 }
--- app/appa/fin_calib/test_fin_calib.c
+++ app/appa/fin_calib/test_fin_calib.c
@@ -24,11 +24,19 @@ Project Includes
 ------------------------------------------------------------------------------*/
 #include "sdrtf_pub.h"
 #include "main.h"
+#include "test.h"
+#include "servo.h"
 
 /*------------------------------------------------------------------------------
 Global Variables 
 ------------------------------------------------------------------------------*/
+
 PRESET_DATA preset_data;
+
+uint8_t usb_test_vals[] = { EXIT };
+
+uint8_t* usb_queue = usb_test_vals;
+uint8_t  usb_pos   = 0;
 
 /*------------------------------------------------------------------------------
 Macros
@@ -37,6 +45,26 @@ Macros
 /*------------------------------------------------------------------------------
 Procedures: Test Helpers
 ------------------------------------------------------------------------------*/
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+*       _preset_to_array        		  			                           *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*       Convert preset servo values to convenient array for testing			   *
+*                                                                              *
+*******************************************************************************/
+uint8_t* _preset_to_array( PRESET_DATA in_preset ) {
+	static uint8_t values[4];
+
+	values[0] = in_preset.servo_preset.rp_servo1;
+	values[1] = in_preset.servo_preset.rp_servo2;
+	values[2] = in_preset.servo_preset.rp_servo3;
+	values[3] = in_preset.servo_preset.rp_servo4;
+
+	return values;
+}
 
 
 /*------------------------------------------------------------------------------
@@ -47,42 +75,52 @@ Procedures: Tests // Define the tests used here
 /*******************************************************************************
 *                                                                              *
 * PROCEDURE:                                                                   * 
-*       test_bar		  				                                       *
+*       test_usb		  				                                       *
 *                                                                              *
 * DESCRIPTION:                                                                 * 
-*       Basic example test													   *
+*       Test any codepaths triggered by a USB command.              		   *
 *                                                                              *
 *******************************************************************************/
-void test_bar 
+void test_usb
 	(
 	void
     )
 {
 /* Step: Set up test */
-#define NUM_CASES_BAR 3
-printf("\nUnit Tests: test_bar\n");
+#define NUM_CASES_USB 8
+printf("\nUnit Tests: test_usb\n");
 
 /* Step: Set up test vectors (inputs, expected) */
-int inputs[NUM_CASES_BAR] = 
+uint8_t inputs[NUM_CASES_USB][2] = 
 {
-#include "cases/blank_inputs.txt"
+#include "cases/usb_input_queue.txt"
 };
 
-int expected[NUM_CASES_BAR] = 
+uint8_t expected[NUM_CASES_USB][4] = 
 {
-#include "cases/blank_expected.txt"
+#include "cases/usb_expected.txt"
 };
 
 /* Step: Execute tests */
-for ( int test_num = 0; test_num < NUM_CASES_BAR; test_num++ )
+for ( int test_num = 0; test_num < NUM_CASES_USB; test_num++ )
 	{
-	/* Call function under test*/
+	usb_queue = inputs[ test_num ];
+	usb_pos = 0;
+
+	uint8_t signalIn;
+
+	finCalibration( &signalIn );
 
 	/* Check result*/
-	TEST_ASSERT_EQ_SINT( "Test that the result equals the expected", 1, 1 );
-	}
+	uint8_t* result = _preset_to_array( preset_data );
 
-} /* test_bar */
+	/* TODO make this more readable */
+	for( int i = 0; i < 4; i++ ) {
+		TEST_ASSERT_EQ_SINT( "Test that servo preset equals expected", expected[test_num][i], result[i] );
+	}
+	//TEST_ASSERT_EQ_SINT( "Test that the result equals the expected", expected[ test_num ] , ( uint8_t[] )  );
+	}
+} /* test_usb */
 
 
 /*******************************************************************************
@@ -104,16 +142,27 @@ int main
 List of things to fix or fake:
 [x] preset_data
 [x] led_set_color
-[ ] motor_drive
-[ ] usb_receive
+[x] motor_drive
+[~] usb_receive
 */
+
+/*------------------------------------------------------------------------------
+Set up global variables
+------------------------------------------------------------------------------*/
+SERVO_PRESET servo_preset;
+servo_preset.rp_servo1 = 0;
+servo_preset.rp_servo2 = 0;
+servo_preset.rp_servo3 = 0;
+servo_preset.rp_servo4 = 0;
+
+preset_data.servo_preset = servo_preset;
 
 /*------------------------------------------------------------------------------
 Test Cases
 ------------------------------------------------------------------------------*/
 unit_test tests[] =
 	{
-	{ "bar", test_bar } /* Callback to function. All you need to do is write a message in a string and the function name! */
+	{ "bar", test_usb } /* Callback to function. All you need to do is write a message in a string and the function name! */
 	};
 
 /*------------------------------------------------------------------------------
